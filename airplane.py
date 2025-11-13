3b7e6a2bfeeef3c1ce9f2ffe291a013bcfa0d740
victory wanjiku <kahihuvictory2@gmail.com>
2025-11-13 14:52:24 +0300

    Update airplane.py

--- airplane.py
+++ airplane.py
@@ -4,7 +4,7 @@ print("\n---SkyLink Air Booking System----")
 passengername=input("enter passengers name:")
 destination=input("enter destination:")
 flightnumber=input("flight number:")
-seatclass=input("ECONOMY","BUSINESS","FIRST CLASS")
+seatclass=input("enter seatclass(ECONOMY/BUSINESS/FIRST CLASS):")
 ticketnumber=input("enter ticket number:")
 if seat_class=="ECONOMY":
          ticketcost=8000
@@ -17,11 +17,12 @@ elif seat_class=="first":
 else:
         ticketnumber="sky{ticket number:001}"
         
-print"\n---SkyLink Air Booking System----"
+print"\n---SkyLink Air Booking System----")
 print("passenger name:",name)
 print("destination:",destination)
 print("flight number:",flightnumber)
 print("seat class:",seatclass)
 print("ticket number:",ticketnumber)
 print("ticket cost:",ticketcost)
+
 
