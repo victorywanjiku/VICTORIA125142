e0435f164f5a96e8ed2ea577b0cbcfea9889540a
victory wanjiku <kahihuvictory2@gmail.com>
2025-11-13 13:44:49 +0300

    Add files via upload

---
+++ airplane.py
@@ -0,0 +1,27 @@
+print("\n---SkyLink Air Booking System----")
+#Input Details
+
+passengername=input("enter passengers name:")
+destination=input("enter destination:")
+flightnumber=input("flight number:")
+seatclass=input("ECONOMY","BUSINESS","FIRST CLASS")
+ticketnumber=input("enter ticket number:")
+if seat_class=="ECONOMY":
+         ticketcost=8000
+ elif seat_class=="BUSINESS":
+         ticketcost=15000
+elif seat_class=="first":
+         ticketcost=15000
+elif seat_class=="first":
+         ticketcost=25000
+else:
+        ticketnumber="sky{ticket number:001}"
+        
+print"\n---SkyLink Air Booking System----"
+print("passenger name:",name)
+print("destination:",destination)
+print("flight number:",flightnumber)
+print("seat class:",seatclass)
+print("ticket number:",ticketnumber)
+print("ticket cost:",ticketcost)
+
